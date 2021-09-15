29b6824fcfdb376883b697dd8846bf4014449fea
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-15 17:09:11 -0300

    remove unnecessary values from similar movies and similar movie dto

---
+++ app/src/main/java/com/desafio/mobile2you/data/model/similarmovies/SimilarMovie.kt
@@ -0,0 +1,13 @@
+package com.desafio.mobile2you.data.model.similarmovies
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SimilarMovie(
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("title")
+    val title: String
+)
--- app/src/main/java/com/desafio/mobile2you/data/model/similarmovies/SimilarMovies.kt
+++ app/src/main/java/com/desafio/mobile2you/data/model/similarmovies/SimilarMovies.kt
@@ -4,12 +4,6 @@ package com.desafio.mobile2you.data.model.similarmovies
 import com.google.gson.annotations.SerializedName
 
 data class SimilarMovies(
-    @SerializedName("page")
-    val page: Int,
     @SerializedName("results")
-    val results: List<Result>,
-    @SerializedName("total_pages")
-    val totalPages: Int,
-    @SerializedName("total_results")
-    val totalResults: Int
+    val similarMovies: List<SimilarMovie>
 )
