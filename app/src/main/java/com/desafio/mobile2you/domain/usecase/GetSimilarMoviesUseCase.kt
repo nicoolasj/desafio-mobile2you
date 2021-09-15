06894a1a60b5ede8a0d621534ea915948c53a5c9
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-15 18:28:37 -0300

    create get similar movies use case

---
+++ app/src/main/java/com/desafio/mobile2you/domain/usecase/GetSimilarMoviesUseCase.kt
@@ -0,0 +1,8 @@
+package com.desafio.mobile2you.domain.usecase
+
+import com.desafio.mobile2you.domain.repository.MovieRepository
+import javax.inject.Inject
+
+class GetSimilarMoviesUseCase @Inject constructor(private val repository: MovieRepository) {
+    suspend fun execute(movieId: String) = repository.getSimilarMovies(movieId)
+}
