92a68001792a86bce8847ea8ebbe23a544d49338
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-15 16:48:21 -0300

    remove unnecessary dto models and values for movie

--- app/src/main/java/com/desafio/mobile2you/data/model/Genre.kt
+++ app/src/main/java/com/desafio/mobile2you/data/model/Genre.kt
@@ -4,8 +4,6 @@ package com.desafio.mobile2you.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Genre(
-    @SerializedName("id")
-    val id: Int,
     @SerializedName("name")
     val name: String
 )
