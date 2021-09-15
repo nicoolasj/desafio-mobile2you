dcb94d065b653fb757e6cfc2305c68eb0cdc56f3
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-15 16:29:56 -0300

    add json to kotlin movie dto model

---
+++ app/src/main/java/com/desafio/mobile2you/data/model/ProductionCountry.kt
@@ -0,0 +1,11 @@
+package com.desafio.mobile2you.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductionCountry(
+    @SerializedName("iso_3166_1")
+    val iso31661: String,
+    @SerializedName("name")
+    val name: String
+)
