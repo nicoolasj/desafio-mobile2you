d5801054f253be14fe12f81883841cb000889557
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 09:56:09 -0300

    create implementation for movie remote data source interface

---
+++ app/src/main/java/com/desafio/mobile2you/data/repository/datasourceimpl/MovieRemoteDataSourceImpl.kt
@@ -0,0 +1,20 @@
+package com.desafio.mobile2you.data.repository.datasourceimpl
+
+import com.desafio.mobile2you.data.api.TMDBService
+import com.desafio.mobile2you.data.model.movie.Movie
+import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
+import com.desafio.mobile2you.data.repository.datasource.MovieRemoteDataSource
+import retrofit2.Response
+
+class MovieRemoteDataSourceImpl(
+    private val tmdbService: TMDBService,
+    private val movieId: String,
+    private val apiKey: String,
+    private val language: String
+) : MovieRemoteDataSource {
+    override suspend fun getMovie(): Response<Movie> =
+        tmdbService.getMovie(movieId, apiKey, language)
+
+    override suspend fun getSimilarMovies(): Response<SimilarMovies> =
+        tmdbService.getSimilarMovies(movieId, apiKey, language)
+}
