81bbf7a15d5a5b0b59134c6fc0c6d26aec2ec736
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 10:37:39 -0300

    remove unnecessary parameters in movie repository interface

--- app/src/main/java/com/desafio/mobile2you/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/desafio/mobile2you/domain/repository/MovieRepository.kt
@@ -4,6 +4,6 @@ import com.desafio.mobile2you.data.model.movie.Movie
 import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
 
 interface MovieRepository {
-    suspend fun getMovie(movieId: String, apiKey: String, language: String): Movie
-    suspend fun getSimilarMovies(movieId: String, apiKey: String, language: String): SimilarMovies
+    suspend fun getMovie(): Movie
+    suspend fun getSimilarMovies(): SimilarMovies
 }
