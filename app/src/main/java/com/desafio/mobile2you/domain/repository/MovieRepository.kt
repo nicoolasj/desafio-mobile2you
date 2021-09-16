59e5de34f44c4d284262870442ccd16032f4005f
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 09:29:48 -0300

    add parameters to get movie and get similar movies based on tmdb service interface update

--- app/src/main/java/com/desafio/mobile2you/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/desafio/mobile2you/domain/repository/MovieRepository.kt
@@ -4,6 +4,6 @@ import com.desafio.mobile2you.data.model.movie.Movie
 import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
 
 interface MovieRepository {
-    suspend fun getMovie(movieId: String): Movie
-    suspend fun getSimilarMovies(movieId: String): SimilarMovies
+    suspend fun getMovie(movieId: String, apiKey: String, language: String): Movie
+    suspend fun getSimilarMovies(movieId: String, apiKey: String, language: String): SimilarMovies
 }
