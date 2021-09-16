c814a4e31a86a438968f77ed0284d4d10741a477
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 09:06:24 -0300

    add missing query parameters in tmdb service interface

--- app/src/main/java/com/desafio/mobile2you/data/api/TMDBService.kt
+++ app/src/main/java/com/desafio/mobile2you/data/api/TMDBService.kt
@@ -5,12 +5,21 @@ import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TMDBService {
 
     @GET("/movie/{movie_id}")
-    suspend fun getMovie(@Path("movie_id") movieId: String): Response<Movie>
+    suspend fun getMovie(
+        @Path("movie_id") movieId: String,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String
+    ): Response<Movie>
 
     @GET("/movie/{movie_id}/similar")
-    suspend fun getSimilarMovies(@Path("movie_id") movieId: String): Response<SimilarMovies>
+    suspend fun getSimilarMovies(
+        @Path("movie_id") movieId: String,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String
+    ): Response<SimilarMovies>
 }
