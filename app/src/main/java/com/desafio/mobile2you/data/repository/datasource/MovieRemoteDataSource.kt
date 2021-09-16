807e6bc65660971a887de5342a95db089f3f789a
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 09:51:19 -0300

    create interface definition for remote data source

---
+++ app/src/main/java/com/desafio/mobile2you/data/repository/datasource/MovieRemoteDataSource.kt
@@ -0,0 +1,11 @@
+package com.desafio.mobile2you.data.repository.datasource
+
+import com.desafio.mobile2you.data.api.TMDBService
+import com.desafio.mobile2you.data.model.movie.Movie
+import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
+import retrofit2.Response
+
+interface MovieRemoteDataSource{
+    suspend fun getMovie(): Response<Movie>
+    suspend fun getSimilarMovies(): Response<SimilarMovies>
+}
