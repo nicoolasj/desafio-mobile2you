6d32bc36ae42be189e7fc20041a6a41bcb443788
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 12:51:45 -0300

    remove base url constant due to a bug in di

--- app/src/main/java/com/desafio/mobile2you/util/Constants.kt
+++ app/src/main/java/com/desafio/mobile2you/util/Constants.kt
@@ -1,7 +1,6 @@
 package com.desafio.mobile2you.util
 
 object Constants {
-    const val BASE_URL = "https://developers.themoviedb.org/3"
     const val MOVIE_ID = "14919"
     const val API_KEY = "bd6a39ec98ccf79b868a85f6550fd51d"
     const val LANGUAGE = "pt-BR"
