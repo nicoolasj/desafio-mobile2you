0533bcbbf2b9b7d1d21278de3b18fec63db7fb09
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 10:38:46 -0300

    remove unnecessary parameters and add movie remote data source as a dependency

--- app/src/main/java/com/desafio/mobile2you/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/desafio/mobile2you/data/repository/MovieRepositoryImpl.kt
@@ -3,20 +3,15 @@ package com.desafio.mobile2you.data.repository
 import com.desafio.mobile2you.data.api.TMDBService
 import com.desafio.mobile2you.data.model.movie.Movie
 import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
+import com.desafio.mobile2you.data.repository.datasource.MovieRemoteDataSource
 import com.desafio.mobile2you.domain.repository.MovieRepository
 import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
-    private val tmdbService: TMDBService,
-) :
-    MovieRepository {
-    override suspend fun getMovie(movieId: String, apiKey: String, language: String): Movie =
-        tmdbService.getMovie(movieId, apiKey, language).body()!!
+    private val movieRemoteDataSource: MovieRemoteDataSource
+) : MovieRepository {
+    override suspend fun getMovie(): Movie = movieRemoteDataSource.getMovie().body()!!
 
-    override suspend fun getSimilarMovies(
-        movieId: String,
-        apiKey: String,
-        language: String
-    ): SimilarMovies =
-        tmdbService.getSimilarMovies(movieId, apiKey, language).body()!!
+    override suspend fun getSimilarMovies(): SimilarMovies =
+        movieRemoteDataSource.getSimilarMovies().body()!!
 }
