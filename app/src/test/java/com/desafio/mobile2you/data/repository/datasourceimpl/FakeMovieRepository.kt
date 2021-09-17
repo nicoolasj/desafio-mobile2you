a6ab6f6f9a7ef12906cbc84ed5f070490a7a0aee
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-16 21:17:01 -0300

    create fake repository for testing

---
+++ app/src/test/java/com/desafio/mobile2you/data/repository/datasourceimpl/FakeMovieRepository.kt
@@ -0,0 +1,40 @@
+package com.desafio.mobile2you.data.repository.datasourceimpl
+
+import com.desafio.mobile2you.data.model.movie.Genre
+import com.desafio.mobile2you.data.model.movie.Movie
+import com.desafio.mobile2you.data.model.similarmovies.SimilarMovie
+import com.desafio.mobile2you.data.model.similarmovies.SimilarMovies
+import com.desafio.mobile2you.domain.repository.MovieRepository
+
+class FakeMovieRepository : MovieRepository {
+
+    private val movie: Movie
+    private val genre = mutableListOf<Genre>()
+    private val similarMovies: SimilarMovies
+    private val similarMovie = mutableListOf<SimilarMovie>()
+
+    init {
+        genre.add(Genre("name"))
+        movie = Movie(
+                "backdropPath",
+                genre,
+                1,
+                "overview",
+                100.0,
+                "posterPath",
+                "tagline",
+                "title",
+                1
+            )
+        similarMovie.add(SimilarMovie("posterPath", "2000", "title"))
+        similarMovies = SimilarMovies(similarMovie)
+    }
+
+    override suspend fun getMovie(): Movie {
+        return movie
+    }
+
+    override suspend fun getSimilarMovies(): SimilarMovies {
+        return similarMovies
+    }
+}
